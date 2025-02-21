0c34f298054f5bfb10e126f89ff1fb04eb3117b7
LEMMIIX <lemmiix@gmail.com>
2025-02-21 23:14:14 +0100

    Language Server Protocol hinzugefügen, vorerst mit: lua, c, cpp

--- lua/lennartsfantastischecnofig/remap.lua
+++ lua/lennartsfantastischecnofig/remap.lua
@@ -2,3 +2,6 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)	-- project view
 
+-- Zeilen verschieben und indent autom. anpassen
+vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv")	-- Zeile runter
+vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv")	-- Zeile hoch
