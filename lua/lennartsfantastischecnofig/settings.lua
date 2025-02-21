1a9e5bc8fe8bae5b153ba21dd1f1cd68330d95b4
LEMMIIX <lemmiix@gmail.com>
2025-02-22 00:38:02 +0100

    anpassen der Farbe für LSP Warnungen

--- lua/lennartsfantastischecnofig/settings.lua
+++ lua/lennartsfantastischecnofig/settings.lua
@@ -1,13 +1,19 @@
 -- eigene settings
 
+-- Misc
+vim.opt.clipboard = "unnamedplus"	-- nvim und sys clipboard sind die selben
+vim.opt.termguicolors = true	-- aktiviert termguicolors
+
 -- indentation
 local length = 4
 vim.opt.tabstop = length
 vim.opt.shiftwidth = length
 vim.opt.softtabstop = length
 
-vim.opt.clipboard = "unnamedplus"	-- nvim und sys clipboard sind die selben
 
 -- Zeilennummern
 vim.opt.number = true
 vim.opt.relativenumber = true -- zeigt relative Zeilennummer von der Position vom cursor
+
+-- Diagnostics
+vim.api.nvim_set_hl(0, 'DiagnosticWarn', { fg = '#d78700' })	-- Setzt WARN Farbe auf orange
