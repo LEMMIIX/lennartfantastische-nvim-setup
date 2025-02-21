f6c38a2674a997ae2fae594e7ac718fbc70ca856
LEMMIIX <lemmiix@gmail.com>
2025-02-21 19:13:24 +0100

    Anzeige der Zeilennummern als einstellung hinzufügen,

--- lua/lennartsfantastischecnofig/settings.lua
+++ lua/lennartsfantastischecnofig/settings.lua
@@ -7,3 +7,7 @@ vim.opt.shiftwidth = length
 vim.opt.softtabstop = length
 
 vim.opt.clipboard = "unnamedplus"	-- nvim und sys clipboard sind die selben
+
+-- Zeilennummern
+vim.opt.number = true
+vim.opt.relativenumber = true -- zeigt relative Zeilennummer von der Position vom cursor
