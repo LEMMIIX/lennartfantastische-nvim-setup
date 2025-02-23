61d74369cead950409550f6c926e4b7b7ef9b2d2
LEMMIIX <lemmiix@gmail.com>
2025-02-23 20:19:41 +0100

    plugins updaten und weitere keyboard shortcuts hinzufügen

--- lua/lennartsfantastischecnofig/remap.lua
+++ lua/lennartsfantastischecnofig/remap.lua
@@ -5,3 +5,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)	-- project view
 -- Zeilen verschieben und indent autom. anpassen
 vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv")	-- Zeile runter
 vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv")	-- Zeile hoch
+
+-- Kopieren/einfügen
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- scheiß Hexenwerk - ausgewähltes Wort lässt simultan alle Vorkommnisse gleichzeitig bearbeiten
+vim.keymap.set("n", "<leader>fr", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
