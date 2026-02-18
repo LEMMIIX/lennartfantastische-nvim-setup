cb31b2499f02109c17c6cb5e53e5d450363f67e2
Lennart Möller <lm@rechenwerk.com>
2026-02-18 17:05:45 +0100

    use root_markers for lua and clangd

--- lua/lennartsfantastischecnofig/lsp/clangd.lua
+++ lua/lennartsfantastischecnofig/lsp/clangd.lua
@@ -65,6 +65,15 @@ end
 return {
 	cmd = { 'clangd' },
 	filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
+	root_markers = {
+		'.clangd',
+		'.clang-tidy',
+		'.clang-format',
+		'compile_commands.json',
+		'compile_flags.txt',
+		'configure.ac', -- AutoTools
+		'.git',
+	},
 	capabilities = {
 		textDocument = {
 			completion = {
